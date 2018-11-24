4308cec55072735e602e599ee428b765e8f2ff46
Michal Charemza <michal@charemza.name>
2018-11-24 16:37:55 +0000

    (release) v0.0.1

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='treelock',
+    version='0.0.1',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='Constant-time read/write sub-tree locking for asyncio Python',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/treelock',
+    py_modules=[
+        'treelock',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'fifolock',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: AsyncIO',
+    ],
+)
