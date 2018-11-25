0415d27398735c31703bc8835eb1adb77a2adfbc
Michal Charemza <michal@charemza.name>
2018-11-25 13:52:23 +0000

    (release) v0.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='treelock',
-    version='0.0.10',
+    version='0.0.11',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Constant-time read/write sub-tree locking for asyncio Python',
