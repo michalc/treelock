ecb60dac2f62418a64ca7c5b212b2cc41001e7b5
Michal Charemza <michal@charemza.name>
2018-11-29 08:00:06 +0000

    (docs) Use of constant-time is misleading

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     version='0.0.12',
     author='Michal Charemza',
     author_email='michal@charemza.name',
-    description='Constant-time read/write sub-tree locking for asyncio Python',
+    description='Fast read/write sub-tree locking for asyncio Python',
     long_description=long_description(),
     long_description_content_type='text/markdown',
     url='https://github.com/michalc/treelock',
